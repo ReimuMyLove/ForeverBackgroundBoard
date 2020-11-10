4ecb85555345024f135ead9b89576f2ed12af1dc
王瑞康 <44659433+lazy-cat-fear@users.noreply.github.com>
2020-11-10 12:22:47 +0800

    框架
    框架

---
+++ project/ShouJieDemo/app/src/main/java/com/example/shoujiedemo/adapter/MyFragmentPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.example.shoujiedemo.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.List;
+
+public class MyFragmentPagerAdapter extends FragmentStateAdapter {
+
+    private List<Fragment> fragmentList;
+
+    public MyFragmentPagerAdapter(@NonNull FragmentActivity fragmentActivity, List<Fragment> fragmetnList) {
+        super(fragmentActivity);
+        this.fragmentList = fragmetnList;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        if(fragmentList.size() > 0){
+            return fragmentList.get(position);
+        }
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        if(fragmentList.size() >0){
+            return fragmentList.size();
+        }
+        return 0;
+    }
+}
---
+++ project/ShouJieDemo/app/src/main/java/com/example/shoujiedemo/fragment/FroundFragment.java
@@ -0,0 +1,37 @@
+package com.example.shoujiedemo.fragment;
+
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.shoujiedemo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FroundFragment extends Fragment {
+
+
+    public FroundFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_fround, container, false);
+        return view;
+    }
+
+
+
+
+}
