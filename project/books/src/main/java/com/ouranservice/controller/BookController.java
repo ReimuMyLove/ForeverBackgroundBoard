09be4b043c262f535c52370d3c7aa116fcd82187
筱邪丶 <44658642+ReimuMyLove@users.noreply.github.com>
2021-05-10 18:03:29 +0800

    Revert "Revert "book接口更新""
    This reverts commit 49ece8a688d327b66bf3a4ee2ffa0c18dafbc830.

--- project/books/src/main/java/com/ouranservice/controller/BookController.java
+++ project/books/src/main/java/com/ouranservice/controller/BookController.java
@@ -2,6 +2,7 @@ package com.ouranservice.controller;
 
 import com.ouranservice.entity.Book;
 import com.ouranservice.service.BookService;
+import com.ouranservice.service.BookTypesService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -14,10 +15,12 @@ import java.util.Set;
 public class BookController {
 
 	final BookService bookService;
+	final BookTypesService bookTypesService;
 
 	@Autowired
-	public BookController(BookService bookService) {
+	public BookController(BookService bookService, BookTypesService bookTypesService) {
 		this.bookService = bookService;
+		this.bookTypesService = bookTypesService;
 	}
 
 	//根据bookId查询简略书籍信息
@@ -39,6 +42,32 @@ public class BookController {
 	@ResponseBody
 	public Set<Book> getAll(){
 		return bookService.getAll();
+	}
+
+	//根据筛选类型条件查询书籍
+	@GetMapping("/byRequest")
+	@ResponseBody
+	public Set<Book> getBookByRequest(@RequestParam("type")String type,
+									  @RequestParam("area")String area,
+									  @RequestParam("year")String year,
+									  @RequestParam("month")String month){
+		//type属性判空
+		int typeId = -1;
+		if (type!=null && !type.equals("")){
+			typeId = bookTypesService.getIdByName(type);
+		}
+		//年月判空
+		String startTime = "",endTime = "";
+		if(year != null && !year.equals("")){		//判断年份是否为空
+			if (month!=null && !month.equals("")){	//判断月份是否为空 若不为空 则开始时间为当前年当前月1日,结束市价哪位当前年当前月31日
+				startTime = year+"-"+month+"-01";
+				endTime   = year+"-"+month+"-28";
+			}else{				//如果月份为空 则默认开始时间为当前年1月1日,结束时间为当前年12月31日
+				startTime = year+"-01-01";
+				endTime   = year+"-12-31";
+			}
+		}
+		return bookService.getBookByRequest(typeId, area,startTime,endTime);
 	}
 
 	//添加书籍
